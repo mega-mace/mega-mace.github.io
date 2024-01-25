2cf71e94ba82ca568cfced6be6163a486a320904
mega-mace <mdanielyt@gmail.com>
2024-01-25 00:09:49 -0700

    Scroll div not page

--- year-in-review/load-top-100.js
+++ year-in-review/load-top-100.js
@@ -2,6 +2,10 @@ function showDropDown() {
     document.getElementById("myDropdown").classList.toggle("show")
 }
 
+function scrollToTop() {
+    document.getElementById("div-body").scrollTop = 0
+}
+
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
         document.getElementById("myDropdown").classList.remove("show")
